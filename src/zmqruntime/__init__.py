cafdea37ed7cfb37e08bf808e74b34d4d54a06eb
Tristan Simas <tristan.simas@mail.mcgill.ca>
2026-02-06 15:44:54 -0500

    bump version to 0.1.6

--- src/zmqruntime/__init__.py
+++ src/zmqruntime/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 from zmqruntime.ack_listener import GlobalAckListener
 from zmqruntime.client import ZMQClient
